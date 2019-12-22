c8246a84462450aa32b1141af42820dc0ba27f07
st <stts.free2802@gmail.com>
2019-12-22 23:52:19 +0900

    記事投稿機能ひな形完成

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,40 @@
+class Admin::PostsController < ApplicationController
+
+  def new
+    @post = current_user.posts.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to post_path(@post), notice: "記事を投稿しました。"
+    else
+      render :new
+    end
+  end
+
+    def edit
+      @post = current_user.posts.find(params[:id])
+    end
+
+    def update
+      @post = current_user.posts.find(params[:id])
+      if @post.update!(post_params)
+        redirect_to post_path(@post), notice: "記事を編集しました。"
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @post = current_user.posts.find(params[:id])
+      @post.destroy
+      redirect_to root_url, notice: "記事「#{@post.title}」を削除しました。"
+    end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :published)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  resources :posts, only: %i[index show]
+  root to: 'tops#index'
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  namespace :admin do
+    get 'home/index'
+    resources :posts, only: %i[new create edit update destroy]
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
