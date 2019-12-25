306682b0d6242f0d94b1e2a00071cfd425750087
sssttt-maker <stst.shu82@gmail.com>
2019-12-25 19:08:41 +0900

    develop s3

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -36,6 +36,6 @@ class Admin::PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :text, :published, :image)
+    params.require(:post).permit(:title, :text, :published, images: [])
   end
 end
