6cc17f90d67522a95e8a86f00d741bcec32f5210
sssttt-maker <stst.shu82@gmail.com>
2020-02-08 15:13:59 +0900

    menuページ作成

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,6 +2,6 @@ class TopsController < ApplicationController
   layout 'top'
   def index
     @galleries = Gallery.all.limit(8)
-    @posts = Post.all.limit(3)
+    @posts = Post.published.limit(3)
   end
 end
